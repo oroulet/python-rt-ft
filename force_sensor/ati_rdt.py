c5439e6c94a1886048a50271aecf981360f80749
Morten Lind <morten@lind.dyndns.dk>
2017-08-24 14:30:57 +0200

    Package for real-time force sensor reading and streaming in Python. Initial simplified re-implementation of the ATI RDT reader with support for streaming and single-shot.

---
+++ force_sensor/ati_rdt.py
@@ -0,0 +1,130 @@
+# coding=utf-8
+
+"""
+"""
+
+__author__ = "Morten Lind"
+__copyright__ = "SINTEF 2017"
+__credits__ = ["Morten Lind"]
+__license__ = "GPLv3"
+__maintainer__ = "Morten Lind"
+__email__ = "morten.lind@sintef.no"
+__status__ = "Development"
+
+import threading
+import socket
+import enum
+import struct
+
+import numpy as np
+
+
+class ATIModes(enum.Enum):
+    STREAM = 2
+    SINGLE = 0
+
+
+class ATIRDT(threading.Thread):
+    """Class for sampling the ATI force sensor over UDP+RDT in both single
+    shot and streaming mode.
+    """
+    
+    REQ_STRUCT = struct.Struct('>HHI')
+    FT_STRUCT = struct.Struct('>IIIiiiiii')
+
+    def __init__(self, rdt_host, rdt_port=49152, bind_host='0.0.0.0'):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self._rdt_addr = (rdt_host, rdt_port)
+        self._bind_addr = (bind_host, rdt_port)#
+        self._sock = socket.socket(type=socket.SOCK_DGRAM)
+        self._sock.settimeout(0.01)
+        self._sock.bind(self._bind_addr)
+        self._mode = ATIModes.SINGLE
+        self._cycle_time = 0.0
+        self._bias = np.zeros(6)
+        self._latest_ft = None
+        self._ft_lock = threading.Lock()
+        self._stream_flag = threading.Event()
+        self.__stop = False
+        self.start()
+        
+    @property
+    def is_streaming(self):
+        return self._mode == ATIModes.STREAM
+
+    @property
+    def is_single(self):
+        return self._mode == ATIModes.SINGLE
+
+    def start_stream(self):
+        if self._mode == ATIModes.STREAM:
+            # Already in streaming mode
+            return False
+        self._sock.sendto(ATIRDT.REQ_STRUCT.pack(0x1234, 2, 0),
+                          self._rdt_addr)
+        self._mode = ATIModes.STREAM
+        self._stream_flag.set()
+        return True
+
+    def stop_stream(self):
+        if self._mode == ATIModes.SINGLE:
+            # Already in single mode
+            return False
+        self._stream_flag.clear()
+        self._sock.sendto(ATIRDT.REQ_STRUCT.pack(0x1234, 0, 0),
+                          self._rdt_addr)
+        self._mode = ATIModes.SINGLE
+        # Empty the receive buffer
+        while self._recv_ft() is not None:
+            pass
+        self._latest_ft = None
+
+        return True
+
+    def zero_bias(self):
+        with self._ft_lock:
+            self._bias = np.zeros(6)
+
+    def sample_bias(self):
+        with self._ft_lock:
+            self._bias = self.ft
+
+    def _recv_ft(self):
+        try:
+            data = ATIRDT.FT_STRUCT.unpack(self._sock.recv(1024))
+        except socket.timeout:
+            return None
+        else:
+            return 1.0e-6 * np.array(data[3:]) - self._bias
+
+    def _req_single_ft(self):
+        self._sock.sendto(ATIRDT.REQ_STRUCT.pack(0x1234, 2, 1),
+                        self._rdt_addr)
+        return self._recv_ft()
+
+    def get_ft(self):
+        if self._mode == ATIModes.STREAM:
+            with self._ft_lock:
+                return self._latest_ft
+        else:
+            # Request and return single measurement
+            return self._req_single_ft()
+            # Receive and return ft value
+            ft = self._recv_ft()
+            for i in range(5):
+                new_ft = self._recv_ft()
+                if new_ft is None:
+                    break
+                else:
+                    ft = new_ft
+            return ft
+        
+    ft = property(get_ft)
+    
+    def run(self):
+        while not self.__stop:
+            self._stream_flag.wait()
+            with self._ft_lock:
+                self._latest_ft = self._recv_ft()
+                    
